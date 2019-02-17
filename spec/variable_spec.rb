641234f5e5eba311d6dc384f90754afc3eb367fc
Dylan Harris <dylanharris23@gmail.com>
2019-02-17 15:02:36 +0000

    Automatically backed up by Learn

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -1,10 +1,17 @@
 # Code your solution in variable.rb
 describe "./variable.rb" do
+<<<<<<< HEAD
   
 
   
     greeting = "Hello World"
     greeting == get_variable_from_file('./variable.rb', "greeting")
+=======
+  greeting = "Hello World"
+
+  
+    greeting = get_variable_from_file('./variable.rb', "greeting")
+>>>>>>> ca2ab3fe9572efe793ae01459b651d7549ddc13f
 
     expect(greeting).to eq("Hello World")
   end
